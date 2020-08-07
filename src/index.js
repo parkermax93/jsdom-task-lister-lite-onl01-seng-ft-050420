00f87bf3c5d90d8eb01a49dd83badf9294c69801
Parker Williamson <parker.williamson@gmail.com>
2020-08-07 18:54:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ const form = document.querySelector('#create-task-form');
 const taskInput
 const 
 
-document.addEventListener("DOMContentLoaded", loadEventListeners() ()
+document.addEventListener("DOMContentLoaded", loadEventListeners());
 
-);
+function loadEventListeners() { 
+  form.addEventListener('submit', addTask);
+}
+
+function addtask(t) {
+  if(taskInput.value === '') { 
+    alert('Add task content');
+  } else { 
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(taskInput.value));
+}
